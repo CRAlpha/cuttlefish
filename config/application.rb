7cbb77c486e7d88f51a7b4b0a8e1ebdb13912f75
Bruce Shi <shibocuhk@gmail.com>
2017-03-15 18:15:32 +0800

    fix domain

--- config/application.rb
+++ config/application.rb
@@ -39,9 +39,9 @@ module Cuttlefish
     config.cuttlefish_domain = ENV["CUTTLEFISH_DOMAIN"] || "cuttlefish.io"
     # TODO Rename the certificate to generic name that doesn't include domain
     config.cuttlefish_domain_cert_chain_file =
-      ENV["CUTTLEFISH_DOMAIN_CERT_CHAIN_FILE"] || "/etc/ssl/cuttlefish.oaf.org.au.pem"
+      ENV["CUTTLEFISH_DOMAIN_CERT_CHAIN_FILE"] || "/etc/ssl/cf.zhaoalpha.pem"
     config.cuttlefish_domain_private_key_file =
-      ENV["CUTTLEFISH_DOMAIN_PRIVATE_KEY_FILE"] || "/etc/ssl/private/cuttlefish.oaf.org.au.key"
+      ENV["CUTTLEFISH_DOMAIN_PRIVATE_KEY_FILE"] || "/etc/ssl/private/cf.zhaoalpha.key"
 
     config.postfix_smtp_host = ENV["POSTFIX_SMTP_HOST"] || "localhost"
     config.postfix_smtp_port = ENV["POSTFIX_SMTP_PORT"] ? ENV["POSTFIX_SMTP_PORT"].to_i : 25
