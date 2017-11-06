a1cbb302909bc86a481bfb54315e7649d0d146ed
eXtremeSHOK <extremeshok@users.noreply.github.com>
2017-11-06 06:04:03 +0200

    Update provision_production.sh
    Ensure the latest galaxy-roles are installed to prevent issues.

--- provision_production.sh
+++ provision_production.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+echo "Updating Ansible galaxy roles"
+ansible-galaxy install -f ANXS.postgresql --roles-path=provisioning/roles/
+ansible-galaxy install -f DavidWittman.redis --roles-path=provisioning/roles/
+ansible-galaxy install -f abtris.nginx-passenger --roles-path=provisioning/roles/
+ansible-galaxy install -f rvm_io.ruby --roles-path=provisioning/roles/
+
+echo "Provisioning"
 cd provisioning
 ansible-playbook -i hosts --vault-password-file=~/.cuttlefish_ansible_vault_pass.txt -u ubuntu --ask-pass --ask-sudo-pass playbook.yml
